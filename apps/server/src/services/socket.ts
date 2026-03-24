b0753fe07e13799155eda740f4cdd66649217a77
Sheryar <alisharyar93@gmail.com>
2026-03-24 19:17:01 +0500

    feat: implementation of socket service with env variables

--- apps/server/src/services/socket.ts
+++ apps/server/src/services/socket.ts
@@ -1,4 +1,17 @@
 import { Server } from "socket.io";
+import { Redis } from "ioredis";
+
+// const redis = new Redis()
+
+const redisConfig = {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+    username: process.env.REDIS_USERNAME,
+    password: process.env.REDIS_PASSWORD,
+};
+
+const pub = new Redis(redisConfig);
+const sub = new Redis(redisConfig);
 
 class SocketService {
     
@@ -31,12 +44,17 @@ class SocketService {
             socket.on("event:message", async ({ message }: { message: string }) => {
             console.log("New Message Rec.", message);
         //  publish this message to redis
-        //  await pub.publish("MESSAGES", JSON.stringify({ message }));
+         await pub.publish("MESSAGES", JSON.stringify({ message }));
       });
     });
 
-            
-        }
+        sub.subscribe("MESSAGES");
+        sub.on("message", (channel, message) => {
+            console.log("New Message from Redis", channel, message);
+            const { message: msg } = JSON.parse(message) as { message: string };
+            io.emit("message", JSON.stringify({ message: msg }));
+        });
+    }
 
         get io() {
             return this._io
