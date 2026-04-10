2e0e5b850d4aa01672f176e06796ea74eab8d6db
Sheryar <alisharyar93@gmail.com>
2026-04-10 20:06:18 +0500

    ` feat: implement Kafka message consumer and integrate with Prisma `

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,9 +1,11 @@
 import http from "http";
 import SocketService from "./services/socket.js";
-// import { startMessageConsumer } from "./services/kafka";
-async function init() {
-//   startMessageConsumer();
-  const socketService = new SocketService();
+import { startMessageConsumer } from "./services/kafka.js";
+
+  async function init() {
+    startMessageConsumer();
+      const socketService = new SocketService();
+
 
   const httpServer = http.createServer();
   const PORT = process.env.PORT ? process.env.PORT : 8000;
--- apps/server/src/services/kafka.ts
+++ apps/server/src/services/kafka.ts
@@ -1,4 +1,6 @@
 import { Kafka, type Producer } from "kafkajs";
+// import { PrismaClient } from "@prisma/client";
+import prismaClient from "./prisma.js";
 import fs from 'fs';
 import path from 'path';
 import dotenv from 'dotenv';
@@ -55,6 +57,34 @@ export async function produceMessage(message: string) {
         topic: process.env.KAFKA_TOPIC || "default_topic",
     });
     return true;
+}
+
+export async function startMessageConsumer() {
+  console.log("Consumer is running..");
+  const consumer = kafka.consumer({ groupId: "default" });
+  await consumer.connect();
+  await consumer.subscribe({ topic: "MESSAGES", fromBeginning: true });
+
+  await consumer.run({
+    autoCommit: true,
+    eachMessage: async ({ message, pause }) => {
+      if (!message.value) return;
+      console.log(`New Message Recv..`);
+      try {
+        await prismaClient.message.create({
+          data: {
+            text: message.value?.toString(),
+          },
+        });
+      } catch (err) {
+        console.log("Something is wrong");
+        pause();
+        setTimeout(() => {
+          consumer.resume([{ topic: "MESSAGES" }]);
+        }, 60 * 1000);
+      }
+    },
+  });
 }
 
 export default kafka;
