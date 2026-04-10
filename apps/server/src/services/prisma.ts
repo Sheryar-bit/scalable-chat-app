431d7fb919ff309808e6f06639990fcd13a9f2fe
Sheryar <alisharyar93@gmail.com>
2026-04-10 19:56:17 +0500

    ` Chore: initilized Prisma client and schema `

--- apps/server/src/services/prisma.ts
+++ apps/server/src/services/prisma.ts
@@ -1,7 +1,16 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
-const prismaClient=new PrismaClient({
-    log: ['query'],
-})
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const adapter = new PrismaPg(pool);
+
+const prismaClient = new PrismaClient({
+  adapter,
+  log: ["query"],
+});
 
 export default prismaClient;
